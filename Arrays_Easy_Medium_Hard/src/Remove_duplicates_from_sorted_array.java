395cfa23a2e90b27c1d9ca1fbfd26d43ebcfe3d5
Bhavan-Prakash <bhavanofficial2004@gmail.com>
2026-07-05 15:16:23 +0530

    5 july 2nd commit

--- Arrays_Easy_Medium_Hard/src/Remove_duplicates_from_sorted_array.java
+++ Arrays_Easy_Medium_Hard/src/Remove_duplicates_from_sorted_array.java
@@ -1,18 +1,39 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Remove_duplicates_from_sorted_array {
 
-    public int[] logic(int[] arr){
-        int size = arr.length;
+  public int[] logic(int[] arr){
+      int size = arr.length;
+      Set<Integer> sets = new HashSet<>();
+      int index = 0;
+      for(int num: arr){
+          if(!sets.contains(num)){
+              sets.add(num);
+              arr[index] = num;
+              index++;
+          }
+      }
+      return arr;
+  }
 
-        for (int i = 0; i < size; i++) {
-            int j = i-1;
-            if(j>=0 && arr[j] == 0){
-                arr[j] = arr[j+1];
-                arr[j+1] = 0;
-            }
-            while(j>=0 && arr[j] == arr[j-1] && j-1>0){
-                arr[j+1] = 0;
-                j--;
-            }
-        }
-    }
+  public static void main(String[] args){
+      int[] arr = {1,1,2,3,4,4,5};
+
+      System.out.println("initial array : ");
+      for(int num : arr){
+          System.out.print(num + " ");
+      }
+      System.out.println();
+
+      Remove_duplicates_from_sorted_array obj = new Remove_duplicates_from_sorted_array();
+
+      int[] soln = obj.logic(arr);
+
+      System.out.println("Answered Array : ");
+      for(int num : soln){
+          System.out.print(num + " ");
+      }
+
+  }
 }
