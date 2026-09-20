75cca560d8cf85d041cf665a4fb6e7f2b6d72cf4
Bhavan-Prakash <bhavanofficial2004@gmail.com>
2026-09-20 11:06:36 +0530

    20 sep

--- Graphs/src/RottenOranges.java
+++ Graphs/src/RottenOranges.java
@@ -16,35 +16,74 @@ class pair{
 
 public class RottenOranges {
 
-    public static void rotten(int[][] input){
+    public static int solution(int[][] input){
         int n = input.length;
+        int t = 1;
         int m = input[0].length;
-
         Queue<pair> qe = new LinkedList<>();
-
-        int[][] vis = new int[n][m];
-
-        for (int i = 0; i < n ; i++) {
+        int[][] bfs = new int[n][m];
+        boolean[][] vis = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if(input[i][j] == 2){
-                    qe.add(new pair(i,j,1));
-                    vis[i][j] = 2;
+                    qe.add(new pair(i,j,t));
+                    bfs[i][j] = 2;
+                    vis[i][j] = true;
+                } else if (input[i][j] == 1) {
+                    bfs[i][j] = 1;
                 }else{
-                    vis[i][j] = 1;
+                    bfs[i][j] = 0;
+                    vis[i][j] = true;
                 }
+
             }
         }
 
-        int[] drow = {-1,0,1,0};
-        int[] dcol = {0,1,0,-1};
-
+        int[] rep_n = {1,0,-1,0};
+        int[] rep_m = {0,-1,0,1};
 
         while (!qe.isEmpty()){
             pair temp = qe.poll();
 
-            if()
+            for (int i = 0; i < 4; i++) {
+                if (
+                        temp.row + rep_n[i] >= 0 &&
+                                temp.row + rep_n[i] < n &&
+                                temp.column + rep_m[i] >= 0 &&
+                                temp.column + rep_m[i] < m &&
+                                !vis[temp.row + rep_n[i]][temp.column + rep_m[i]] &&
+                                input[temp.row + rep_n[i]][temp.column + rep_m[i]] == 1
+                ){
+                    t+=1;
+                    bfs[temp.row+rep_n[i]][temp.column+rep_m[i]] = 2;
+                    vis[temp.row+rep_n[i]][temp.column+rep_m[i]] = true;
+                    qe.add(new pair(temp.row+rep_n[i],temp.column+rep_m[i], t));
+                }
+            }
 
         }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (bfs[i][j] == 1 || !vis[i][j]){
+                    return -1;
+                }
+            }
+        }
+
+        return t;
+
+    }
+
+    public static void main(String[] args){
+        int[][] input ={
+                {2,1,1},
+                {0,2,2},
+                {2,0,1}
+        };
+
+        System.out.println("solution is : " + solution(input));
+
     }
 
 }
